456149886cb44ba398cbb1250857dbf8e9905435
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2018-05-02 17:36:29 +0300

    final changes in index.js

--- js/module-02/js/index.js
+++ js/module-02/js/index.js
@@ -1,15 +1,15 @@
-"use strict";
-
 let userInput;
 const numbers = [];
 let total = 0;
 
 do {
-  userInput = prompt("Введіть число");
-  if (Number.isNaN(Number(userInput)) || userInput === "" || userInput === null) {
-    alert("Введено не число, спробуйте ще раз!");
+  userInput = prompt('Введіть число');
+  if (userInput === null) {
+    break;
   }
-  else {
+  if (Number.isNaN(Number(userInput)) || userInput === '') {
+    alert('Введено не число, спробуйте ще раз!');
+  } else {
     numbers.push(Number(userInput));
   }
 } while (userInput !== null);
@@ -17,4 +17,7 @@ do {
 for (let i = 0; i < numbers.length; i = i + 1) {
   total = total + numbers[i];
 }
-alert(`Загальна сума дорівнює: ${total}`);
+
+if (numbers.length >= 1) {
+  alert(`Загальна сума дорівнює: ${total}`);
+}
