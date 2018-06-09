a988c732f2433e23e5da4b50eaf32fb26ece830e
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2018-06-09 20:02:55 +0300

    prepared everything for work

--- js/module-06/js/index.js
+++ js/module-06/js/index.js
@@ -6,3 +6,29 @@ class Hamburger {
     this.stuffing = stuffing;
   }
 }
+
+Hamburger.SIZE_SMALL = 'SIZE_SMALL';
+Hamburger.SIZE_LARGE = 'SIZE_LARGE';
+
+Hamburger.SIZES = {
+  [Hamburger.SIZE_SMALL]: { price: 30, calories: 50 },
+  [Hamburger.SIZE_LARGE]: { price: 50, calories: 100 },
+};
+
+Hamburger.STUFFING_CHEESE = 'STUFFING_CHEESE';
+Hamburger.STUFFING_SALAD = 'STUFFING_SALAD';
+Hamburger.STUFFING_MEAT = 'STUFFING_MEAT';
+
+Hamburger.STUFFINGS = {
+  [Hamburger.STUFFING_CHEESE]: { price: 15, calories: 20 },
+  [Hamburger.STUFFING_SALAD]: { price: 20, calories: 15 },
+  [Hamburger.STUFFING_MEAT]: { price: 35, calories: 15 },
+};
+
+Hamburger.TOPPING_SPICE = 'TOPPING_SPICE';
+Hamburger.TOPPING_SAUCE = 'TOPPING_SAUCE';
+
+Hamburger.TOPPINGS = {
+  [Hamburger.TOPPING_SPICE]: { price: 10, calories: 0 },
+  [Hamburger.TOPPING_SAUCE]: { price: 15, calories: 5 },
+};
