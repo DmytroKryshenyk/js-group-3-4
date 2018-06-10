dbe1a1206aa59278995c8508728c868b9e63ce28
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2018-06-10 16:28:09 +0300

    wrote hamburger cashier

--- js/module-06/js/index.js
+++ js/module-06/js/index.js
@@ -4,6 +4,49 @@ class Hamburger {
   constructor(size, stuffing) {
     this.size = size;
     this.stuffing = stuffing;
+    this.toppings = [];
+  }
+
+  addTopping(topping) {
+    if (!this.toppings.includes(topping)) {
+      this.toppings.push(topping);
+    }
+  }
+
+  get calculateCalories() {
+    return (
+      Hamburger.SIZES[this.size].calories +
+      Hamburger.STUFFINGS[this.stuffing].calories +
+      this.toppings.reduce(function(acc, current) {
+        return acc + Hamburger.TOPPINGS[current].calories;
+      }, 0)
+    );
+  }
+
+  get calculatePrice() {
+    return (
+      Hamburger.SIZES[this.size].price +
+      +Hamburger.STUFFINGS[this.stuffing].price +
+      this.toppings.reduce(function(acc, current) {
+        return acc + Hamburger.TOPPINGS[current].price;
+      }, 0)
+    );
+  }
+
+  get getSize() {
+    return this.size;
+  }
+
+  removeTopping(removeTopping) {
+    this.toppings = this.toppings.filter(current => current !== removeTopping);
+  }
+
+  get getToppings() {
+    return this.toppings;
+  }
+
+  get getStuffing() {
+    return this.stuffing;
   }
 }
 
@@ -32,3 +75,36 @@ Hamburger.TOPPINGS = {
   [Hamburger.TOPPING_SPICE]: { price: 10, calories: 0 },
   [Hamburger.TOPPING_SAUCE]: { price: 15, calories: 5 },
 };
+
+// Маленький гамбургер с начинкой из сыра
+const hamburger = new Hamburger(
+  Hamburger.SIZE_SMALL,
+  Hamburger.STUFFING_CHEESE,
+);
+
+// Добавка из приправы
+hamburger.addTopping(Hamburger.TOPPING_SPICE);
+
+// Спросим сколько там калорий
+console.log('Calories: ', hamburger.calculateCalories);
+
+// Сколько стоит?
+console.log('Price: ', hamburger.calculatePrice);
+
+// Я тут передумал и решил добавить еще соус
+hamburger.addTopping(Hamburger.TOPPING_SAUCE);
+
+console.log('Calories: ', hamburger.calculateCalories);
+// // А сколько теперь стоит?
+console.log('Price with sauce: ', hamburger.calculatePrice);
+
+// Проверить, большой ли гамбургер?
+console.log('Is hamburger large: ', hamburger.getSize === Hamburger.SIZE_LARGE); // -> false
+
+// Убрать добавку
+hamburger.removeTopping(Hamburger.TOPPING_SPICE);
+
+// // Смотрим сколько добавок
+console.log('Hamburger has %d toppings', hamburger.getToppings.length); // 1
+
+console.log(`Hamburgers stuffing is ${hamburger.getStuffing}`);
