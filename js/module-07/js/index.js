58b66c2634b6ba8b4f5c0b10179c9c89534125e5
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2018-06-24 17:21:41 +0300

    finished to create module-07

--- js/module-07/js/index.js
+++ js/module-07/js/index.js
@@ -1,45 +1,83 @@
+'use strict';
+
+// createPostCard(
+//   "http://via.placeholder.com/400x150",
+//   "New article",
+//   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos."
+// );
+
 /*
-  Создайте функцию createPostCard(), которая
-  создает и возвращает DOM-узел карточки поста.
+  1. Модифицируйте готовую функцию createPostCard() из задания
+    номер 6 (https://codepen.io/goit-fe-adv/pen/MVPaeZ) так,
+    чтобы она принимала объект post с данными для заполнения полей
+    в карточке.
 
-  Разметка с классами есть на вкладке HTML.
-  Стили на вкладке CSS.
+  2. Создайте функцию createCards(posts), которая принимает массив
+    объектов-карточек, вызывает функцию createPostCard(post) столько
+    раз, сколько объектов в массиве, сохраняя общий результат и возвращает
+    массив DOM-элементов всех постов.
 
-  Используйте createElement для создания узлов.
-  Добавьте классы и атрибуты.
+  3. Повесьте все посты в какой-то уже существующий DOM-узел.
 */
-"use strict";
-const body = document.querySelector("body");
 
-function createPostCard(postImageLink, post_Title, post_Text) {
+const posts = [
+  {
+    img: 'https://placeimg.com/400/150/arch',
+    title: 'Post title 1',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique!',
+    link: 'link-1.com',
+  },
+  {
+    img: 'https://placeimg.com/400/150/nature',
+    title: 'Post title 2',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo.',
+    link: 'link-2.com',
+  },
+  {
+    img: 'https://placeimg.com/400/150/people',
+    title: 'Post title 3',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur!',
+    link: 'link-3.com',
+  },
+];
 
-  const post = document.createElement("div");
-  post.classList.add("post");
+//===============================================
 
-  const postImage = document.createElement("img");
-  postImage.classList.add("post__image");
-  postImage.setAttribute("src", postImageLink);
-  postImage.setAttribute("alt", "post image");
+const body = document.querySelector('body');
 
-  const postTitle = document.createElement("h2");
-  postTitle.classList.add("post__title");
-  postTitle.textContent = post_Title;
+function createCards(posts) {
+  posts.forEach(element => {
+    createPostCard(element);
+  });
+}
 
-  const postText = document.createElement("p");
-  postText.classList.add("post__text");
-  postText.textContent = post_Text;
+function createPostCard(element) {
+  const post = document.createElement('div');
+  post.classList.add('post');
 
-  const button = document.createElement("a");
-  button.classList.add("button");
-  button.textContent = "Read more";
-  button.setAttribute("href", "#");
+  const postImage = document.createElement('img');
+  postImage.classList.add('post__image');
+  postImage.setAttribute('src', element.img);
+  postImage.setAttribute('alt', 'post image');
+
+  const postTitle = document.createElement('h2');
+  postTitle.classList.add('post__title');
+  postTitle.textContent = element.title;
+
+  const postText = document.createElement('p');
+  postText.classList.add('post__text');
+  postText.textContent = element.text;
+
+  const button = document.createElement('a');
+  button.classList.add('button');
+  button.textContent = 'Read more';
+  button.setAttribute('href', element.link);
 
   body.appendChild(post);
   post.append(postTitle, postImage, postText, button);
 }
 
-createPostCard(
-  "http://via.placeholder.com/400x150",
-  "New article",
-  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos."
-);
+createCards(posts);
