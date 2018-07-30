ca3aa4c5772ee2b0f55b1617279085fdcf0553ad
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2018-07-30 21:53:32 +0300

    finished making a filter

--- js/module-11/js/index.js
+++ js/module-11/js/index.js
@@ -91,8 +91,8 @@ const laptops = [
   },
 ];
 
-let filtredLaptops;
-const filter = { size: [], color: [], release_date: [] };
+let filtredLaptops = []; //сюди загрузаться обєкти товарів, які пройшли фільтрацію
+const filter = { size: [], color: [], release_date: [] }; // тут обєкт із значеннями чекнутих інпутів
 
 const form = document.querySelector('.js-form');
 const productsWrapper = document.querySelector('.products-wraper');
@@ -101,38 +101,77 @@ const source = document.getElementById('product-card-shablon').innerHTML.trim();
 const template = Handlebars.compile(source);
 
 form.addEventListener('submit', filterFn);
+form.addEventListener('reset', resetForm);
 
 function filterFn(event) {
   event.preventDefault();
-  filtredLaptops = [];
-  const checkedInputs = Array.from(form.querySelectorAll('.js-form input:checked'));
+  const checkedInputs = Array.from(form.querySelectorAll('.js-form input:checked')); //масив чекнутих інпутів
 
-  checkedInputs.forEach(element => {
-    const name = element.name;
-    const value = element.value;
-    filter[name].push(value);
-  });
-
+  fillingFilterValues(checkedInputs)
   productsFiltering(laptops);
 
   const markup = template(filtredLaptops);
   productsWrapper.innerHTML = markup;
+
+  resetFilter()
 }
 
 
-// ===============================================================
+// Функція заповнення обєкта filter вибраними значеннями інпутів
+function fillingFilterValues(checkedInputs) {
+  checkedInputs.forEach(element => {
+    const name = element.name;
+    const value = element.value;
+    filter[name].push(value);
+  })
+}
+
+// Функція, яка створює масив обєктів, які відповідають значенням вибраних інпутів
 function productsFiltering(laptopsList) {
   laptopsList.forEach(product => {
+    // Тут ниже три флаги, які відповідають чи підходить товар вибраним інпутам, якщо true значить товар підходить по цьому параметру
     let appropriateSize = false;
     let appropriateColor = false;
     let appropriateReleaseDate = false;
 
-    filter.size.forEach(value => {if (value === String(product.size)) appropriateSize = true;});
+    //Ниже три if , які дозволяють показати товар, якщо якийсь із інпутів взагалі не був обраний
+    if (filter.size.length === 0) appropriateSize = true;
+    if (filter.color.length === 0) appropriateColor = true;
+    if (filter.release_date.length === 0) appropriateReleaseDate = true;
 
-    filter.color.forEach(value => {if (value === product.color) appropriateColor = true;});
+    // фільтруєм по розміру
+    filter.size.forEach(value => {
+      if (value === String(product.size)) 
+      appropriateSize = true;
+    });
 
-    filter.release_date.forEach(value => {if (value === String(product.release_date)) appropriateReleaseDate = true;});
+    // фільтруєм по польору
+    filter.color.forEach(value => {
+      if (value === product.color) 
+      appropriateColor = true;
+    });
 
+    // фільтруєм по року випуску
+    filter.release_date.forEach(value => {
+      if (value === String(product.release_date)) 
+      appropriateReleaseDate = true;
+    });
+
+    // якщо по всім параметрам у нас true значить товар проходить фільтрацію
     if (appropriateSize && appropriateColor && appropriateReleaseDate) filtredLaptops.push(product);
   });
+}
+
+// reset всьої форми
+function resetForm(event) {
+  resetFilter();
+  form.reset();
+}
+
+// reset тільки фільтра
+function resetFilter() {
+  filter.size = [];
+  filter.color = [];
+  filter.release_date = [];
+  filtredLaptops = []; //очищаєм масив фільтравоних обєктів від попередньої фільтрації
 }
