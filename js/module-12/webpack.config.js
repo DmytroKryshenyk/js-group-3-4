841c0b5e2171ca086a74964282aee0fd6978c563
Kryshenyk Dmytro <KryshenykDmytro@gmail.com>
2018-08-15 18:46:16 +0300

    create module-12 branch

---
+++ js/module-12/webpack.config.js
@@ -0,0 +1,40 @@
+var path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'buildStyle.css', //це імя результуючого файла, де в нас буде зберігатись весь наш css
+    }),
+  ],
+};
